e3940611dcc6a7cf94d27eb5f8b9389cf00f4827
Pam Afaneh <pamela.afaneh@gmail.com>
2022-11-09 14:29:36 -0600

    Add files via upload

--- src/data/data.js
+++ src/data/data.js
@@ -1,23 +1,47 @@
-import GlxTravel from '../assets/projects/glx-travel.jpg'
-import Next from '../assets/projects/next.jpg'
+import GlxTravel from "../assets/projects/glx-travel.jpg";
+import Next from "../assets/projects/next.jpg";
+import Data from "../assets/projects/data-analytics.jpg";
+import Flexis from "../assets/projects/flexis.jpg";
+import Door from "../assets/projects/chore-door.jpg";
 
-export const data=[
-    {
-        id:1,
-        name:"React JS Router Application",
-        image:GlxTravel,
-        github:"https://github.com/pammie89/To-The-Stars-Travel.git",
-        live:"https://pammie89.github.io/To-The-Stars-Travel/",
-    },
+export const data = [
+  {
+    id: 1,
+    name: "React JS Router Application",
+    image: GlxTravel,
+    github: "https://github.com/pammie89/To-The-Stars-Travel.git",
+    live: "https://pammie89.github.io/To-The-Stars-Travel/",
+  },
 
-    {
-        id:2,
-        name:"HTML, CSS & Javascript Site",
-        image:Next,
-        github:"https://github.com/pammie89/Next-VR.git",
-        live:"https://pammie89.github.io/Next-VR/",
-    },
-    
+  {
+    id: 2,
+    name: "HTML, CSS & Javascript Site",
+    image: Next,
+    github: "https://github.com/pammie89/Next-VR.git",
+    live: "https://pammie89.github.io/Next-VR/",
+  },
 
+  {
+    id: 3,
+    name: "React JS Development",
+    image: Data,
+    github: "https://github.com/pammie89/Data.git",
+    live: "https://pammie89.github.io/Data/",
+  },
 
-]
+  {
+    id: 4,
+    name: "React Self Built App",
+    image: Flexis,
+    github: "https://github.com/pammie89/flexis.git",
+    live: "https://pammie89.github.io/flexis/",
+  },
+
+  {
+    id: 5,
+    name: "Chore Door - JS Coded",
+    image: Door,
+    github: "https://github.com/pammie89/chore-door.git",
+    live: "https://pammie89.github.io/chore-door/",
+  },
+];
